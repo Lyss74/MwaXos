dee02dfa1353d77b9086e36283ff8c92a5acbff7
Lyss74 <Selyan11032013>
2021-09-24 11:14:09 +0200

    Init repository

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# MwaXos
-Artiste | Auteur · Compositeur · Interprète
+![Logo of the project](https://zupimages.net/up/21/38/ay9o.jpg)
+
+# MwaXos | Moi-même | Site officiel #
+# AUTEUR · COMPOSITEUR · INTERPRÈTE #
+
+## Développement du projet →
+	→ Url du repository : 			https://github.com/Lyss74/MwaXos#readme
+	→ Accueil du site : 			https://github.com/Lyss74/index.html
+	→ Descriptif du site : 			https://github.com/Lyss74/page/descriptif.html
+	→ Choix typographique du site : https://github.com/Lyss74/choiceFonts.html
+
+## Durèe de concéption →
+	→ Premiers jet d'un site vitrine et référencement ( 2 mois )
+	→ Platefaorme d'écoute, publicitè, shop ( 12 mois )
+
+## Référencement sur Google →
+	→ Description de l’artiste
+	→ Mots-clef top position
+	→ Suivi et maintien du projet ...
+
+## Partage sur les réseaux sociaux ( à définir ) →
+	→ Facebook
+	→ Spotify
+	→ Instagram...
+
+## Projet en cours →
+	→ `Putain d'AmbolieS`
+	→ url : 						https://github.com/Lyss74/page/projets.html
+
+## Page en cours de construction →
+# Contact
+# Plateforme d’écoute
+# Boutique
+# Playlist
+
+## TO DO list :
+# THEME →
+	→ Quatre couleurs favorites
+	→ Deux typograhies
+	→ Création d'un logo
+	→ https://www.dafont.com/fr/theme.php?cat=606 (dafont.com)
+
+# Mise en relation des réseaux sociaux
+
+# Texte biographique →
+	→ Ville natale
+	→ Concert, victoire trophèes d'art et jeune artiste
+
+# Article de presse →
+	→ https://www.ladepeche.fr/article/2017/06/21/2597752-maxos-l-artiste-qui-fait-raper-le-tarn.html
---
+++ public/assets/js/scroll.js
@@ -0,0 +1,116 @@
+/* SCROLL HOME PAGE START */
+window.onscroll = function() {scrollHomeFunction()};
+function scrollHomeFunction() {
+    if (document.body.scrollTop > 6600 || document.documentElement.scrollTop > 6600)
+    {
+        document.getElementById("haut").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("haut").style.display = "none";
+    }
+}
+/* SCROLL HOME PAGE END */
+
+/* SCROLL SHOP PAGE START */
+window.onscroll = function() {scrollShopFunction()};
+function scrollShopFunction() {
+    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
+    {
+        document.getElementById("haut").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("haut").style.display = "none";
+    }
+}
+/* SCROLL SHOP PAGE END */
+
+/* SCROLL CHOICE-TYPO START */
+window.onscroll = function() {scrollTypoFunction()};
+function scrollTypoFunction() {
+    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
+    {
+        document.getElementById("haut").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("haut").style.display = "none";
+    }
+}
+/* SCROLL CHOICE-TYPO PAGE END */
+
+/* SCROLL CONTACT PAGE START */
+window.onscroll = function() {scrollContactFunction()};
+function scrollContactFunction() {
+    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
+    {
+        document.getElementById("haut").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("haut").style.display = "none";
+    }
+}
+/* SCROLL CONTACT PAGE END */
+
+/* SCROLL DESCR PAGE START */
+window.onscroll = function() {scrollDescrFunction()};
+function scrollDescrFunction() {
+    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
+    {
+        document.getElementById("haut").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("haut").style.display = "none";
+    }
+}
+/* SCROLL DESCR PAGE END */
+
+/* SCROLL LISTEN PAGE START */
+window.onscroll = function() {scrollListenFunction()};
+function scrollListenFunction() {
+    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
+    {
+        document.getElementById("haut").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("haut").style.display = "none";
+    }
+}
+/* SCROLL LISTEN PAGE END */
+
+/* SCROLL PLAYLIST PAGE START */
+window.onscroll = function() {scrollPlaylistFunction()};
+function scrollPlaylistFunction() {
+    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
+    {
+        document.getElementById("haut").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("haut").style.display = "none";
+    }
+}
+/* SCROLL PLAYLIST PAGE START */
+
+/* SCROLL PROJECTS PAGE START */
+window.onscroll = function() {scrollProjectsFunction()};
+function scrollProjectsFunction() {
+    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
+    {
+        document.getElementById("haut").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("haut").style.display = "none";
+    }
+}
+/* SCROLL PROJECTS PAGE START */
+
+function retourHaut() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+} 
