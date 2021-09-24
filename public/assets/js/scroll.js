346b6cbf4cd4a1041df09f7e9c8e5a4c61a39c31
Lyss74 <Selyan11032013>
2021-09-24 13:21:30 +0200

    Fix theme V1.0

--- public/assets/js/scroll.js
+++ public/assets/js/scroll.js
@@ -18,104 +18,20 @@ function returnHomeUp() {
 
 /*
 /!* SCROLL SHOP PAGE START *!/
-window.onscroll = function() {scrollShopFunction()};
-function scrollShopFunction() {
-    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
-    {
-        document.getElementById("haut").style.display = "block";
-    }
-    else
-    {
-        document.getElementById("haut").style.display = "none";
-    }
-}
 /!* SCROLL SHOP PAGE END *!/
 
 /!* SCROLL CHOICE-TYPO START *!/
-window.onscroll = function() {scrollTypoFunction()};
-function scrollTypoFunction() {
-    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
-    {
-        document.getElementById("haut").style.display = "block";
-    }
-    else
-    {
-        document.getElementById("haut").style.display = "none";
-    }
-}
 /!* SCROLL CHOICE-TYPO PAGE END *!/
 
 /!* SCROLL CONTACT PAGE START *!/
-window.onscroll = function() {scrollContactFunction()};
-function scrollContactFunction() {
-    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
-    {
-        document.getElementById("haut").style.display = "block";
-    }
-    else
-    {
-        document.getElementById("haut").style.display = "none";
-    }
-}
-/!* SCROLL CONTACT PAGE END *!/
-
-/!* SCROLL DESCR PAGE START *!/
-window.onscroll = function() {scrollDescrFunction()};
-function scrollDescrFunction() {
-    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
-    {
-        document.getElementById("haut").style.display = "block";
-    }
-    else
-    {
-        document.getElementById("haut").style.display = "none";
-    }
-}
 /!* SCROLL DESCR PAGE END *!/
 
 /!* SCROLL LISTEN PAGE START *!/
-window.onscroll = function() {scrollListenFunction()};
-function scrollListenFunction() {
-    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
-    {
-        document.getElementById("haut").style.display = "block";
-    }
-    else
-    {
-        document.getElementById("haut").style.display = "none";
-    }
-}
 /!* SCROLL LISTEN PAGE END *!/
 
 /!* SCROLL PLAYLIST PAGE START *!/
-window.onscroll = function() {scrollPlaylistFunction()};
-function scrollPlaylistFunction() {
-    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
-    {
-        document.getElementById("haut").style.display = "block";
-    }
-    else
-    {
-        document.getElementById("haut").style.display = "none";
-    }
-}
-/!* SCROLL PLAYLIST PAGE START *!/
+/!* SCROLL PLAYLIST PAGE END *!/
 
 /!* SCROLL PROJECTS PAGE START *!/
-window.onscroll = function() {scrollProjectsFunction()};
-function scrollProjectsFunction() {
-    if (document.body.scrollTop > 1255 || document.documentElement.scrollTop > 1255)
-    {
-        document.getElementById("haut").style.display = "block";
-    }
-    else
-    {
-        document.getElementById("haut").style.display = "none";
-    }
-}
-/!* SCROLL PROJECTS PAGE START *!/
-
-function retourHaut() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-} */
+/!* SCROLL PROJECTS PAGE END *!/
+ */
