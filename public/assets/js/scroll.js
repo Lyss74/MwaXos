7bb175879f480056adb6dd6d330b5aa4ecf7a2a5
Lyss74 <Selyan11032013>
2021-10-13 14:18:07 +0200

    Fix the first page

--- public/assets/js/scroll.js
+++ public/assets/js/scroll.js
@@ -1,13 +1,13 @@
 /* SCROLL HOME PAGE START */
 window.onscroll = function() {scrollFunctionHome()};
 function scrollFunctionHome() {
-    if (document.body.scrollTop > 6835 || document.documentElement.scrollTop > 6835)
+    if (document.body.scrollTop > 7660 || document.documentElement.scrollTop > 7660)
     {
-        document.getElementById("haut").style.display = "block";
+        document.getElementById("upHome").style.display = "block";
     }
     else
     {
-        document.getElementById("haut").style.display = "none";
+        document.getElementById("upHome").style.display = "none";
     }
 }
 function returnHomeUp() {
@@ -16,128 +16,141 @@ function returnHomeUp() {
 }
 /* SCROLL HOME PAGE END */
 
-/* SCROLL SHOP PAGE START */
+/*
+/!* SCROLL SHOP PAGE START *!/
 window.onscroll = function() {scrollFunctionShop()};
 function scrollFunctionShop() {
-    if (document.body.scrollTop > 1250 || document.documentElement.scrollTop > 1250)
+    if (document.body.scrollTop > 560 || document.documentElement.scrollTop > 560)
     {
-        document.getElementById("haut").style.display = "block";
+        document.getElementById("upShop").style.display = "block";
     }
     else
     {
-        document.getElementById("haut").style.display = "none";
+        document.getElementById("upShop").style.display = "none";
     }
 }
 function returnShopUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-/* SCROLL SHOP PAGE END */
+/!* SCROLL SHOP PAGE END *!/
+*/
 
-/* SCROLL CHOICE-TYPO START */
+/*
+/!* SCROLL CHOICE-TYPO START *!/
 window.onscroll = function() {scrollFunctionChoice()};
 function scrollFunctionChoice() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300)
     {
-        document.getElementById("haut").style.display = "block";
+        document.getElementById("upChoice").style.display = "block";
     }
     else
     {
-        document.getElementById("haut").style.display = "none";
+        document.getElementById("upChoice").style.display = "none";
     }
 }
 function returnChoiceUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-/* SCROLL CHOICE-TYPO PAGE END */
+/!* SCROLL CHOICE-TYPO PAGE END *!/
+*/
 
-/* SCROLL CONTACT PAGE START */
+/*
+/!* SCROLL CONTACT PAGE START *!/
 window.onscroll = function() {scrollFunctionContact()};
 function scrollFunctionContact() {
     if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400)
     {
-        document.getElementById("haut").style.display = "block";
+        document.getElementById("upContact").style.display = "block";
     }
     else
     {
-        document.getElementById("haut").style.display = "none";
+        document.getElementById("upContact").style.display = "none";
     }
 }
 function returnContactUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-/* SCROLL CONTACT PAGE END */
+/!* SCROLL CONTACT PAGE END *!/
+*/
 
-/* SCROLL DESCR PAGE START */
+/*
+/!* SCROLL DESCR PAGE START *!/
 window.onscroll = function() {scrollFunctionDescr()};
 function scrollFunctionDescr() {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500)
     {
-        document.getElementById("haut").style.display = "block";
+        document.getElementById("upDescr").style.display = "block";
     }
     else
     {
-        document.getElementById("haut").style.display = "none";
+        document.getElementById("upDescr").style.display = "none";
     }
 }
 function returnDescrUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-/* SCROLL DESCR PAGE END */
+/!* SCROLL DESCR PAGE END *!/
+*/
 
-/* SCROLL LISTEN PAGE START returnListenUp */
+/*
+/!* SCROLL LISTEN PAGE START returnListenUp *!/
 window.onscroll = function() {scrollFunctionListen()};
 function scrollFunctionListen() {
     if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600)
     {
-        document.getElementById("haut").style.display = "block";
+        document.getElementById("upListen").style.display = "block";
     }
     else
     {
-        document.getElementById("haut").style.display = "none";
+        document.getElementById("upListen").style.display = "none";
     }
 }
 function returnListenUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-/* SCROLL LISTEN PAGE END */
+/!* SCROLL LISTEN PAGE END *!/
+*/
 
-/* SCROLL PLAYLIST PAGE START */
+/*
+/!* SCROLL PLAYLIST PAGE START *!/
 window.onscroll = function() {scrollFunctionPlaylist()};
 function scrollFunctionPlaylist() {
     if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700)
     {
-        document.getElementById("haut").style.display = "block";
+        document.getElementById("upPlaylist").style.display = "block";
     }
     else
     {
-        document.getElementById("haut").style.display = "none";
+        document.getElementById("upPlaylist").style.display = "none";
     }
 }
 function returnPlaylistUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-/* SCROLL PLAYLIST PAGE END */
+/!* SCROLL PLAYLIST PAGE END *!/
+*/
 
-/* SCROLL PROJECTS PAGE START */
+/*
+/!* SCROLL PROJECTS PAGE START *!/
 window.onscroll = function() {scrollFunctionProject()};
 function scrollFunctionProject() {
     if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800)
     {
-        document.getElementById("haut").style.display = "block";
+        document.getElementById("upProjects").style.display = "block";
     }
     else
     {
-        document.getElementById("haut").style.display = "none";
+        document.getElementById("upProjects").style.display = "none";
     }
 }
 function ReturnProjectUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-/* SCROLL PROJECTS PAGE END */
+/!* SCROLL PROJECTS PAGE END *!/*/
